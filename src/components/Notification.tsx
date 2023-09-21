331e74a58ab1242c26eb17b14e710bf56b74df37
Felice Forgione <forgione67@gmail.com>
2023-09-20 20:55:06 -0400

    Added menu component

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Notification() {
-  return <div>Notification</div>;
+  return (
+    <div className="h-10 px-4 bg-red-700 text-white text-center flex justify-center items-center text-sm md:text-base">
+      Free delivery on orders over $25
+    </div>
+  );
 }
 
 export default Notification;
