feabd8bad2277862454f42b524c33d11dee19af5
Felice Forgione <forgione67@gmail.com>
2023-09-22 08:01:47 -0400

    Added phone button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import logo from "../../public/logo.png";
 import Link from "next/link";
 import { MenuLink } from "@/entities/MenuLink";
 import CartIcon from "./CartIcon";
+import { RiPhoneFill } from "react-icons/ri";
 
 const LINKS: MenuLink[] = [
   { id: 1, title: "Home", url: "/" },
@@ -17,6 +18,9 @@ function Navbar() {
     <div className="h-12 p-4 flex justify-between items-center border-b-2">
       <div className="relative w-32 h-8">
         <Image src={logo} alt="logo" fill />
+      </div>
+      <div className="w-36 bg-yellow rounded-xl flex p-1 gap-1 justify-center items-center ">
+        <RiPhoneFill /> <span>888-8888</span>
       </div>
       <div className="hidden md:flex gap-4">
         {LINKS.map((link) => (
