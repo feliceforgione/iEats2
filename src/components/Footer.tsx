ec4f6b9a4f7188d1b322adadf2c1a2454680842e
Felice Forgione <forgione67@gmail.com>
2023-09-22 08:07:56 -0400

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="h-12 p-4 lg-p-20 flex items-center justify-between">
+      <Link href="/" className="font-bold">
+        iEats
+      </Link>
+      <p>ALL RIGHTS RESERVED</p>
+    </div>
+  );
 }
 
 export default Footer;
