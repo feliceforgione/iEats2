2d4b8fdb7dec2bebf4b2d5ce79f90303f2c9397f
Felice Forgione <forgione67@gmail.com>
2023-09-24 17:46:32 -0400

    Created category listing page

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,7 +1,38 @@
+import { pizza } from "@/data";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function CategoryPage() {
-  return <div>CategoryPage</div>;
+  return (
+    <div className="flex flex-wrap">
+      {pizza.map((item) => (
+        <Link
+          href={`/product/${item.id}`}
+          key={item.id}
+          className="w-full h-[60vh] border-r-2 border-b-2 border-red-400 p-4 flex flex-col justify-between group even:bg-orange-50 sm:w-1/2 lg:w-1/3"
+        >
+          {item.img && (
+            <div className="relative h-[80%]">
+              <Image
+                src={item.img}
+                alt=""
+                fill
+                className="object-contain"
+              ></Image>
+            </div>
+          )}
+          <div className="flex items-center justify-between font-bold">
+            <h1>{item.title}</h1>
+            <h2 className="group-hover:hidden">${item.price}</h2>
+            <button className="hidden uppercase bg-yellow p-2 rounded-lg group-hover:block">
+              Add to Cart
+            </button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default CategoryPage;
--- src/data.ts
+++ src/data.ts
@@ -67,6 +67,61 @@ export const featuredProducts: Products = [
   },
 ];
 
+export const pizza: Products = [
+  {
+    id: 1,
+    title: "Margherita",
+    description: "fresh mozzarella, tomato sauce, basil",
+    img: "/images/food/margherita.png",
+    price: 11,
+    options: [
+      {
+        title: "Personal",
+        additionalPrice: 0,
+      },
+      {
+        title: "Large",
+        additionalPrice: 7,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Classic",
+    description: "regular mozzarella, tomato sauce",
+    img: "/images/food/classicpizza.png",
+    price: 11,
+    options: [
+      {
+        title: "Personal",
+        additionalPrice: 0,
+      },
+      {
+        title: "Large",
+        additionalPrice: 7,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Hot Honey",
+    description:
+      "soppressata, honey with habanero peppers, mozzarella, tomato sauce",
+    img: "/images/food/hothoney.png",
+    price: 16,
+    options: [
+      {
+        title: "Personal",
+        additionalPrice: 0,
+      },
+      {
+        title: "Large",
+        additionalPrice: 9,
+      },
+    ],
+  },
+];
+
 type Menu = {
   id: number;
   slug: string;
