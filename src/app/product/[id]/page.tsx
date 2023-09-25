a5952a439934151f5a405ab0a2d7e6867cbecc2a
Felice Forgione <forgione67@gmail.com>
2023-09-25 18:01:03 -0400

    Updated Product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,26 @@
+import { singleProduct } from "@/data";
+import Image from "next/image";
 import React from "react";
 
 function ProductPage() {
-  return <div>ProductPage</div>;
+  return (
+    <div className="p-4 lg:px-20 h-screen flex flex-col justify-around md:flex-row">
+      <div className="relative">
+        {singleProduct.img && (
+          <Image
+            src={singleProduct.img}
+            alt=""
+            className="object-contain"
+            fill
+          />
+        )}
+      </div>
+      <div>
+        <h1>{singleProduct.title}</h1>
+        <p>{singleProduct.description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
--- src/data.ts
+++ src/data.ts
@@ -122,6 +122,24 @@ export const pizza: Products = [
   },
 ];
 
+export const singleProduct: Product = {
+  id: 1,
+  title: "Margherita",
+  description: "fresh mozzarella, tomato sauce, basil",
+  img: "/images/food/margherita.png",
+  price: 11,
+  options: [
+    {
+      title: "Personal",
+      additionalPrice: 0,
+    },
+    {
+      title: "Large",
+      additionalPrice: 7,
+    },
+  ],
+};
+
 type Menu = {
   id: number;
   slug: string;
