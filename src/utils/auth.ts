0cf412165bdc25f0a1304d82de7cd42f0cff4f29
Felice Forgione <forgione67@gmail.com>
2023-09-30 14:29:19 -0400

    Added prisma adapter and user models

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,13 @@
 import { NextAuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
