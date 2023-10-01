b8bbeee21bf9d15f706c2a6c5b8e91b017564628
Felice Forgione <forgione67@gmail.com>
2023-10-01 02:07:55 -0400

    Product page updated to use api route

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,31 +1,39 @@
 import Price from "@/components/Price";
-import { singleProduct } from "@/data";
+import { Product } from "@/entities/ProductType";
 import Image from "next/image";
 import React from "react";
 
-function ProductPage() {
+const getData = async (id: string) => {
+  const resp = await fetch(
+    `http://${process.env.SERVER_DOMAIN}:${process.env.SERVER_PORT}/api/products/${id}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!resp.ok) {
+    throw new Error("Product fetch failed");
+  }
+
+  return resp.json();
+};
+
+async function ProductPage({ params }: { params: { id: string } }) {
+  const product: Product = await getData(params.id);
+
   return (
     <div className="p-4 lg:px-20 h-screen flex flex-col justify-around md:flex-row md:gap-8 md:items-center">
       <div className="relative w-full h-1/2 md:h-[70%]">
-        {singleProduct.img && (
-          <Image
-            src={singleProduct.img}
-            alt=""
-            className="object-contain"
-            fill
-          />
+        {product.img && (
+          <Image src={product.img} alt="" className="object-contain" fill />
         )}
       </div>
       <div className="h-1/2 flex flex-col gap-3 md:h-[70%] md:justify-center md:gap-6 lg:gap-8 ">
         <h1 className="text-3xl font-bold uppercase xl:text-4xl">
-          {singleProduct.title}
+          {product.title}
         </h1>
-        <p>{singleProduct.description}</p>
-        <Price
-          id={singleProduct.id}
-          price={singleProduct.price}
-          options={singleProduct.options}
-        />
+        <p>{product.description}</p>
+        <Price product={product} />
       </div>
     </div>
   );
