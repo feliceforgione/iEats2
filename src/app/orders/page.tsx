dc458f8ab978cc1ca5d76fc20f508e43cfb8e35d
Felice Forgione <forgione67@gmail.com>
2023-10-01 00:19:31 -0400

    Updated order page to include admin actions

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,6 +1,55 @@
+"use client";
 import React from "react";
+import { OrderType } from "@/entities/OrderType";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { RiEdit2Line } from "react-icons/ri";
 
 function OrdersPage() {
+  const { data: session, status } = useSession();
+
+  const router = useRouter();
+
+  if (status === "unauthenticated") {
+    router.push("/login");
+  }
+
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["orders"],
+    queryFn: () =>
+      fetch("http://localhost:3000/api/orders").then((res) => res.json()),
+  });
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: ({ id, status }: { id: string; status: string }) => {
+      return fetch(`http://localhost:3000/api/orders/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(status),
+      });
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["orders"] });
+    },
+  });
+
+  const handleUpdate = (e: React.FormEvent<HTMLFormElement>, id: string) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const input = form.elements[0] as HTMLInputElement;
+    const status = input.value;
+
+    mutation.mutate({ id, status });
+  };
+
+  if (isLoading || status === "loading") {
+    return <span>Loading...</span>;
+  }
   return (
     <div className="p-4 lg:px-20 xl:px-30">
       <table className="w-full">
@@ -14,25 +63,34 @@ function OrdersPage() {
           </tr>
         </thead>
         <tbody>
-          <tr className="text-sm md:text-base odd:bg-gray-100">
-            <td className="hidden md:block py-6 px-1">123456</td>
-            <td className="py-6 px-1">01/12/2023</td>
-            <td className="py-6 px-1">50.00</td>
-            <td className="hidden py-6 px-1 md:block">
-              Margarita Pizza Large (3)
-            </td>
-            <td className="py-6 px-1">On the way</td>
-          </tr>
-
-          <tr className="text-sm md:text-base odd:bg-gray-100">
-            <td className="hidden md:block py-6 px-1">123455</td>
-            <td className="py-6 px-1">01/11/2023</td>
-            <td className="py-6 px-1">30.00</td>
-            <td className="hidden py-6 px-1 md:block">
-              Margarita Pizza Large (2)
-            </td>
-            <td className="py-6 px-1">Delivered</td>
-          </tr>
+          {data.map((item: OrderType) => (
+            <tr className="text-sm md:text-base odd:bg-gray-100" key={item.id}>
+              <td className="hidden md:block py-6 px-1">{item.id}</td>
+              <td className="py-6 px-1">
+                {item.createdAt.toString().slice(0, 10)}
+              </td>
+              <td className="py-6 px-1">${item.price}</td>
+              <td className="hidden py-6 px-1 md:block">
+                {JSON.stringify(item.products)}
+              </td>
+              {session?.user.isAdmin ? (
+                <td>
+                  <form
+                    className="flex items-center justify-center gap-4"
+                    onSubmit={(e) => handleUpdate(e, item.id)}
+                  >
+                    <input
+                      placeholder={item.status}
+                      className="p-2 ring-1 ring-yellow rounded-lg"
+                    />
+                    <RiEdit2Line />
+                  </form>
+                </td>
+              ) : (
+                <td className="py-6 px-1">{item.status}</td>
+              )}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
