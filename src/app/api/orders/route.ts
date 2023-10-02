ea147dc41b3fde3ac2f6564d908520910fbb4308
Felice Forgione <forgione67@gmail.com>
2023-10-02 00:36:23 -0400

    Added stripe api routes

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -34,3 +34,33 @@ export const GET = async (req: NextRequest) => {
     );
   }
 };
+
+export const POST = async (req: NextRequest) => {
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      const body = await req.json();
+
+      const order = await prisma.order.create({
+        data: body,
+      });
+      return new NextResponse(JSON.stringify(order), { status: 201 });
+    } catch (error) {
+      console.log(error);
+      return new NextResponse(
+        JSON.stringify({
+          message: `Error: ${error}`,
+        }),
+        { status: 500 }
+      );
+    }
+  } else {
+    return new NextResponse(
+      JSON.stringify({
+        message: `Not authenticated`,
+      }),
+      { status: 401 }
+    );
+  }
+};
